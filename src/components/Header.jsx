3e02e071604637b29b9ac6922ef9fdb2a73c39c5
Yana Doneva <ydoneva@theoremus.com>
2025-07-29 10:44:43 +0300

    translation cosmetics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -90,7 +90,7 @@ function MobileNavigation(props) {
   return (
     <Popover {...props}>
       <Popover.Button className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
-        Menu
+        {t('menu.title')}
         <ChevronDownIcon className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
       </Popover.Button>
       <Transition.Root>
@@ -123,7 +123,7 @@ function MobileNavigation(props) {
                 <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
               </Popover.Button>
               <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
-                Navigation
+                {t('menu.nav')}
               </h2>
             </div>
             <nav className="mt-6">
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -64,6 +64,8 @@ function SocialLink({ icon: Icon, ...props }) {
 
 
 function Newsletter() {
+    const {t} = useTranslation('common')
+
   return (
     <form
       action="https://app.kit.com/forms/8302059/subscriptions"
@@ -73,10 +75,10 @@ function Newsletter() {
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Subscribe</span>
+        <span className="ml-3">   {t('subscribe.title')}</span>
       </h2>
       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when we publish something new and unsubscribe at any time.
+           {t('subscribe.description')}
       </p>
       <div className="mt-6 flex">
         <input
@@ -88,7 +90,7 @@ function Newsletter() {
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
         />
         <Button type="submit" className="ml-4 flex-none">
-          Join
+             {t('subscribe.button')}
         </Button>
       </div>
     </form>
